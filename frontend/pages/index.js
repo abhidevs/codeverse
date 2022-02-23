a7b3bec9f60688f0b83b42f7d4fa632e4cd722e2
Abhik Das <abhikdas98765@gmail.com>
2022-02-23 21:14:57 +0530

    initial commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home | Codeverse</title>
+        <meta name="description" content="Social media for programmers" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Codeverse</h1>
+      </main>
+    </div>
+  );
+}
