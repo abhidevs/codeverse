bd4f1e1174d0ddca98f85b749fcd1e64c6b753ab
Abhik Das <abhikdas98765@gmail.com>
2022-03-27 21:52:04 +0530

    fixed image not found error

--- frontend/data/data.js
+++ frontend/data/data.js
@@ -17,7 +17,7 @@ const userData = [
 
         name: 'Abhik Das',
         username: '@avhik03',
-        profile: '/profile1.webp',
+        profile: '/profile1.jpg',
         description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters",
         postImage: '/image4.jpg',
         liked:true,
