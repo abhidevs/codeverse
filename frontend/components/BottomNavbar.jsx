c6aad3395c7c1ba7ab0e02fd95b361d9f683ff81
Abhik Das <abhikdas98765@gmail.com>
2022-03-27 17:02:59 +0530

    added animation in BottomNavbar

--- frontend/components/BottomNavbar.jsx
+++ frontend/components/BottomNavbar.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import {
   HomeIcon,
   SearchIcon,
@@ -11,12 +12,44 @@ import { useRouter } from "next/router";
 const BottomNavbar = () => {
   const router = useRouter();
 
+  useEffect(() => {
+    const links = [...document.querySelectorAll("nav ul li a")];
+    const light = document.querySelector("nav .tubelight");
+
+    let activeIndex = 0;
+    let prevActiveIndex = 0;
+
+    links.forEach((link, index) => {
+      if (links[index].classList.contains("active")) {
+        light.style.left = `${
+          links[index].offsetLeft + light.offsetWidth / 4
+        }px`;
+        activeIndex = index;
+        prevActiveIndex = index;
+      }
+
+      link.addEventListener("click", (e) => {
+        activeIndex = index;
+
+        if (activeIndex !== prevActiveIndex) {
+          e.target.classList.add("active");
+          links[prevActiveIndex].classList.remove("active");
+
+          prevActiveIndex = activeIndex;
+          light.style.left = `${
+            links[index].offsetLeft + light.offsetWidth / 4
+          }px`;
+        }
+      });
+    });
+  }, []);
+
   return (
     <nav
-      className="lg:hidden bg-skin-backgroud text-skin-base fixed left-0 bottom-0 w-full h-[60px] 
-        border-t flex justify-center items-center overflow-hidden z-10"
+      className="lg:hidden bg-skin-backgroud text-skin-base fixed left-0 -bottom-1 w-full h-[60px] 
+        border-t flex justify-center items-center overflow-hidden z-10 px-[15px]"
     >
-      <ul className="w-full max-w-[500px] h-full grid grid-cols-5">
+      <ul className="min-w-[350px] h-full grid grid-cols-5">
         <BottomNavbarLink
           icon={HomeIcon}
           link="/"
@@ -43,6 +76,13 @@ const BottomNavbar = () => {
           active={router.pathname == "/bookmarks"}
         />
       </ul>
+
+      <div
+        className="tubelight absolute left-0 top-0 w-[45px] h-[5px] rounded-[5px] bg-skin-secondary 
+        transition-all duration-[400ms] ease-linear z-10"
+      >
+        <div className="absolute left-[-30%] top-[5px] w-[160%] h-[60px] light-ray"></div>
+      </div>
     </nav>
   );
 };
