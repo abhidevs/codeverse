47d415f1a9e5760a468f6c6f6ff10d8da37df5b7
Abhik Das <abhikdas98765@gmail.com>
2022-05-17 18:30:43 +0530

    fixed typo in backend controllers

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const authController = require("../controlers/authController");
+const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/register", authController.registerUser);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const userController = require("../controlers/userController");
+const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
